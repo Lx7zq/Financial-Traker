44fbbb93febcfe8a6c36b99404fd0caec24b803a
prempree deelert <prempreeez@gmail.com>
2024-09-16 12:43:29 +0700

    16/9/

--- fontend/src/main.jsx
+++ fontend/src/main.jsx
@@ -10,7 +10,7 @@ const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key");
 }
-
+//grg
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
